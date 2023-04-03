0b936663f80b4adbc5798b75c84e147a8a7d2c18
baohusanvip <128116972+baohusanvip@users.noreply.github.com>
2023-04-03 22:21:19 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -103,9 +103,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>ChatGPT中国-永久免费</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            加入AI交流群，微信swint12
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
